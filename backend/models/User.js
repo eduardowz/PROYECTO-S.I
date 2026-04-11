bee5649c3b1b5cc873c507e6254754c58b05297f
eduardowz <sanjuaneduardo748@gmail.com>
2026-04-11 02:51:31 -0600

    agregar verificationToken y tokenExpira a User

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,34 +1,31 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-    tipo:         { type: String, required: true },
-    nombre:       { type: String, required: true },
-    correo:       { type: String, unique: true, required: true },
-    telefono:     String,
-    password:     { type: String, required: true },
-    edad:         String,
-    ubicacion:    String,
-    rfc:          String,
-    sitioWeb:     String,
-    direccion:    String,
-    estado:       { type: String, default: "activo" },
-    fechaRegistro:{ type: String, default: () => new Date().toLocaleString() },
-    notificaciones:  { type: Boolean, default: false },
-    cvSubido:        { type: Boolean, default: false },
-    postulaciones:   { type: Array,   default: [] },
-    verificada:      { type: Boolean, default: false },
+    tipo:          { type: String, required: true },
+    nombre:        { type: String, required: true },
+    correo:        { type: String, unique: true, required: true },
+    telefono:      String,
+    password:      { type: String, required: true },
+    edad:          String,
+    ubicacion:     String,
+    rfc:           String,
+    sitioWeb:      String,
+    direccion:     String,
+    estado:        { type: String, default: "activo" },
+    fechaRegistro: { type: String, default: () => new Date().toLocaleString() },
+    notificaciones:    { type: Boolean, default: false },
+    cvSubido:          { type: Boolean, default: false },
+    postulaciones:     { type: Array,   default: [] },
+    verificada:        { type: Boolean, default: false },
+    verificationToken: { type: String,  default: null },
+    tokenExpira:       { type: Date,    default: null },
 
     cv: {
-    nombreCompleto: String,
-    telefono:       String,
-    habilidades:    String,
-    experiencia:    String
+        nombreCompleto: String,
+        telefono:       String,
+        habilidades:    String,
+        experiencia:    String
     }
-
-    
-
 });
-
-
 
 module.exports = mongoose.model("User", UserSchema);
