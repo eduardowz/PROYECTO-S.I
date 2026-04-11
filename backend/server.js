4fa2fadcfa3f3c172c9833388ace111cf8365ae9
eduardowz <sanjuaneduardo748@gmail.com>
2026-04-11 01:19:37 -0600

    puerto fijo 3000

--- backend/server.js
+++ backend/server.js
@@ -35,6 +35,6 @@ app.get("/", (req, res) => {
     res.send("Servidor funcionando");
 });
 
-app.listen(process.env.PORT || 3000, () => {
-    console.log("Servidor en puerto", process.env.PORT || 3000);
+app.listen(3000, () => {
+    console.log("Servidor en puerto 3000");
 });
